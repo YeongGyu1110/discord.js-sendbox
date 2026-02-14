700ff55a0fd32d5dad01f66cece5aa12c5c8a6b6
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2026-02-15 02:45:55 +0900

    이거까지만 하고: getUserData를 구현한다

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ const { token } = require('./token.js');
 
 client.login(token);
 
+let userData = {};
+
+function getUserData(message) {
+    const sender = message.author.id;
+    if (!userData[sender]) {
+        userData[sender] = {
+            id: sender,
+            name: message.author.username
+        };
+    }
+    return userData[sender];
+}
+
 // 그래서 async가 뭔데
 client.on("messageCreate", async (message) => {
 
     // 메시지 작자가 bot이라면 message.author.bot이 true가 되는데 그렇다면 이 익명 함수 전체를 끝내버리기.
     if (message.author.bot) return;
+
+    // 메모리 영역에 저장되는 유저 객체 생성
+    let user = getUserData(message);
 
     // 메시지 전체 소문자 변경, 앞 뒤 공백 제거
     const msg = message.content.toLocaleLowerCase().trim();
