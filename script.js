77228c3fbe05c33703ff95b17e76e5b8ebdfd06c
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2026-02-15 01:50:24 +0900

    초기 설정: discord.js를 설치하고 기타 초기 설정을 완료해 시작을 준비함

---
+++ script.js
@@ -0,0 +1,8 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
+const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
+const { token } = require('./token.js');
+
+client.login(token);
+
+client.once('clientReady', () => console.log('hello, world!'));
