6406b1e786d9015c25cc94a5b76f6d8759c9d7d0
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2026-02-17 02:17:10 +0900

    아오 드르븐거 드디어 완료: fs

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
-const kiwiMode = true;
-
 const { Client, GatewayIntentBits } = require('discord.js');
 const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
 const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
+const kiwiMode = false;
 const { token } = require(kiwiMode ? './token_kiwi.js' : './token.js');
 
-client.login(token);
 
+const fs = require('fs');
+// 파일 이름만 덜렁 있으면 작업 폴더 바로 아래에 생성됨.
+// 경로 달면 해당하는 경로에 파일 생김
+const fileName = './userDataBase/userData.json';
 let userData = {};
+if (fs.existsSync(fileName)) {
+    userData = loadUserData();
+}
+function saveUserData(userData) {
 
-function getUserData(message) {
-    const sender = message.author.id;
-    if (!userData[sender]) {
-        userData[sender] = {
-            id: sender,
-            name: message.author.username
+    fs.writeFileSync(`${fileName}`, JSON.stringify(userData, null, 2));
+}
+function loadUserData() {
+    return JSON.parse(fs.readFileSync(fileName, 'utf-8'));
+}
+function getUserData(userId, username) {
+    if (!userData[userId]) {
+        userData[userId] = {
+            id: userId,
+            name: username,
+            money: 0
         };
+        saveUserData(userData);
+    } else {
+        console.log(`유저에 대한 값이 이미 있습니다!`);
     }
-    return userData[sender];
+    return userData[userId];
 }
 
-// 그래서 async가 뭔데
 client.on("messageCreate", async (message) => {
-
-    // 메시지 작자가 bot이라면 message.author.bot이 true가 되는데 그렇다면 이 익명 함수 전체를 끝내버리기.
     if (message.author.bot) return;
-
-    // 메모리 영역에 저장되는 유저 객체 생성
-    let user = getUserData(message);
-
-    // 메시지 전체 소문자 변경, 앞 뒤 공백 제거
     const msg = message.content.toLocaleLowerCase().trim();
-    // message.channel.send에서 .channel 제거해서 message.send으로 간략하게 하기
     message.send = (content) => message.channel.send(content);
+    const userId = message.author.id;
 
+    // 로그인 여부 보여주기
+    if (msg == "login") message.send(`${!!userData[message.author.id]}`);
+    if (msg == 'create') {
+        getUserData(message.author.id, message.author.username);
+        message.send(`작업 완료`);
+    }
+
+    if (!userData[userId]) return; // 유저 데이터에 userHash에 대한 값이 없다면 즉시 종료
+    // 즉 이 아래는 유저 데이터가 있어야 작동하는 코드.
+
+    var user = userData[userId];
+    if (msg == "hello") message.send(`hello.`);
+
+    saveUserData(userData);
 });
 
 client.once('clientReady', () => console.log(kiwiMode ? 'KIWI KIWI' : 'default mode on!'));
+client.login(token);
