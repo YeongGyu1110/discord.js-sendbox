c4e229d1875240d7930517db1cf7e405095ba6cd
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2026-02-26 18:59:49 +0900

    style: 불필요한 주석을 삭제하고 코드 스타일을 개선한다.

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
 const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
 const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
-const kiwiMode = false;
+const kiwiMode = true;
 const { token } = require(kiwiMode ? './token_kiwi.js' : './token.js');
 
 
-const fs = require('fs');
-// 파일 이름만 덜렁 있으면 작업 폴더 바로 아래에 생성됨.
-// 경로 달면 해당하는 경로에 파일 생김
-const fileName = './userDataBase/userData.json';
 let userData = {};
+
+const fileName = './userDataBase/userData.json';
+
 if (fs.existsSync(fileName)) {
     userData = loadUserData();
 }
-function saveUserData(userData) {
 
+function saveUserData(userData) {
     fs.writeFileSync(`${fileName}`, JSON.stringify(userData, null, 2));
 }
+
 function loadUserData() {
     return JSON.parse(fs.readFileSync(fileName, 'utf-8'));
 }
+
 function getUserData(userId, username) {
     if (!userData[userId]) {
         userData[userId] = {
@@ -40,15 +42,13 @@ client.on("messageCreate", async (message) => {
     message.send = (content) => message.channel.send(content);
     const userId = message.author.id;
 
-    // 로그인 여부 보여주기
     if (msg == "login") message.send(`${!!userData[message.author.id]}`);
     if (msg == 'create') {
         getUserData(message.author.id, message.author.username);
         message.send(`작업 완료`);
     }
 
-    if (!userData[userId]) return; // 유저 데이터에 userHash에 대한 값이 없다면 즉시 종료
-    // 즉 이 아래는 유저 데이터가 있어야 작동하는 코드.
+    if (!userData[userId]) return;
 
     var user = userData[userId];
     if (msg == "hello") message.send(`hello.`);
