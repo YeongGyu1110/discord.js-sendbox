ddfa9e78ec3eb4a96a0f5b7ff6b3ea3085e91819
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2026-03-14 18:17:32 +0900

    style: DB 객체를 DATA로 rename하고 deepCopy와 getUserData를 DATA의 메소드로 지정한다.

--- script.js
+++ script.js
@@ -5,14 +5,27 @@ const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
 const kiwiMode = true;
 const { token } = require(kiwiMode ? './token_kiwi.js' : './token.js');
 
-const DB = {};
+const DATA = {};
 
-DB.saveData = function(userData) {
+DATA.saveData = function(userData) {
     fs.writeFileSync(`${fileName}`, JSON.stringify(userData, null, 2));
 }
-
-DB.loadData = function() {
+DATA.loadData = function() {
     return JSON.parse(fs.readFileSync(fileName, 'utf-8'));
+}
+DATA.copyUserData = function(target) {
+    return JSON.parse(JSON.stringify(target));
+}
+DATA.getUserData = function(userId, username) {
+    if (!userData[userId]) {
+        userData[userId] = DATA.copyUserData(defualUserData);
+        userData[userId].name = username;
+        userData[userId].id = userId;
+        DATA.saveData(userData);
+    } else {
+        console.log(`유저에 대한 값이 이미 있습니다!`);
+    }
+    return userData[userId];
 }
 
 let userData = {};
@@ -20,7 +33,7 @@ let userData = {};
 const fileName = './userDataBase/userData.json';
 
 if (fs.existsSync(fileName)) {
-    userData = DB.loadData();
+    userData = DATA.loadData();
 }
 
 const defualUserData = {
@@ -28,31 +41,16 @@ const defualUserData = {
     level: 1
 }
 
-function deepCopy(target) {
-    return JSON.parse(JSON.stringify(target));
-}
-
-function getUserData(userId, username) {
-    if (!userData[userId]) {
-        userData[userId] = deepCopy(defualUserData);
-        userData[userId].name = username;
-        userData[userId].id = userId;
-        DB.saveData(userData);
-    } else {
-        console.log(`유저에 대한 값이 이미 있습니다!`);
-    }
-    return userData[userId];
-}
-
 client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
     const msg = message.content.toLocaleLowerCase().trim();
     message.send = (content) => message.channel.send(content);
     const userId = message.author.id;
+    
 
     if (msg == "login") message.send(`${!!userData[message.author.id]}`);
     if (msg == 'create') {
-        getUserData(message.author.id, message.author.username);
+        DATA.getUserData(message.author.id, message.author.username);
         message.send(`작업 완료`);
     }
 
@@ -61,7 +59,7 @@ client.on("messageCreate", async (message) => {
     var user = userData[userId];
     if (msg == "hello") message.send(`hello.`);
 
-    DB.saveData(userData);
+    DATA.saveData(userData);
 });
 
 client.once('clientReady', () => console.log(kiwiMode ? 'KIWI KIWI' : 'default mode on!'));
